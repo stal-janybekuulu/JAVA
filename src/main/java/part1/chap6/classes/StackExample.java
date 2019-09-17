2b1e8a821ee4a1ba796273402345c79c90c3c170
gustav <stal.janybekuulu@gmail.com>
2019-09-17 20:09:54 +0600

    star

---
+++ src/main/java/part1/chap6/classes/StackExample.java
@@ -0,0 +1,36 @@
+package part1.chap6.stack;
+
+/*
+В этом классе определяется целочисленный стек,
+в котором можно хранить до 10 челочисленных значений
+ */
+public class StackExample {
+    int stck[] = new int[10];
+    int tos;
+
+    //Инициализировать вершину стека
+    StackExample() {
+        tos = -1;
+    }
+
+    //Разместить элемент в стеке
+    void push(int item) {
+        if (tos == 9) {
+            System.out.println("Стек заполнен.");
+        }
+        else {
+            stck[++tos] = item;
+        }
+    }
+
+    //Извлечь элемент из стека
+    int pop() {
+        if (tos < 0) {
+            System.out.println("Стек не загружен.");
+            return 0;
+        }
+        else {
+            return stck[tos--];
+        }
+    }
+}
---
+++ src/main/java/part1/chap6/classes/TestStack.java
@@ -0,0 +1,27 @@
+package part1.chap6.stack;
+
+public class TestStack {
+    public static void main(String[] args) {
+        StackExample mystack1 = new StackExample();
+        StackExample mystack2 = new StackExample();
+
+        //Разместить числа в стеке
+        for (int i = 0; i < 10; i++) {
+            mystack1.push(i);
+        }
+        for (int i = 10; i < 20; i++) {
+           mystack2.push(i);
+        }
+
+        //Извлечь эти числа из стека
+        System.out.println("Содержимое стека mystack1: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(mystack1.pop());
+        }
+
+        System.out.println("Содержимое стека mystack2: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(mystack2.pop());
+        }
+    }
+}
